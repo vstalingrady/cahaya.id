50aba9325dc70b21b4f08826a25881a77e31d8f2
Vstalin Grady <VstalinGrady@gmail.com>
2025-06-30 15:03:45 +0000

    something like poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
       </head>
