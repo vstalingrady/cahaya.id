58d02757f6d5f2c1374b8952cf6f57e28aa02a43
Vstalin Grady <VstalinGrady@gmail.com>
2025-07-02 11:34:22 +0000

    put it back to purple please. black black black black black black purple

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function MainAppLayout({
       <NoiseOverlay opacity={0.02} />
       <div className="absolute inset-0 -z-10 h-full w-full bg-background">
         <div className="absolute -top-1/2 left-0 right-0 h-full bg-[radial-gradient(ellipse_50%_50%_at_50%_0%,hsl(262_80%_58%/0.15),transparent_70%)]"></div>
-        <div className="absolute -bottom-1/2 left-0 right-0 h-full bg-[radial-gradient(ellipse_50%_50%_at_50%_100%,hsl(350_85%_60%/0.15),transparent_70%)]"></div>
+        <div className="absolute -bottom-1/2 left-0 right-0 h-full bg-[radial-gradient(ellipse_50%_50%_at_50%_100%,hsl(0_72%_51%/0.15),transparent_70%)]"></div>
       </div>
 
       <main className="flex-1 overflow-y-auto p-6 relative z-10 pb-24">
