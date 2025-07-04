2067ceba95b809bc85a1bb0aa9e97d7588525c6e
Vstalin Grady <VstalinGrady@gmail.com>
2025-07-04 02:58:27 +0000

    starting build "5d75a577-f958-457d-b8ef-5cf0cb904ed6" FETCHSOURCE BUILD

---
+++ workspace/src/app/verify-phone/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import VerifyPhoneForm from '@/components/auth/verify-phone-form';
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+export default function VerifyPhonePage() {
+  return (
+    <div className="w-full max-w-md mx-auto bg-background text-white p-6 flex flex-col justify-center min-h-screen">
+        <div className="text-center mb-8">
+            <h1 className="text-4xl font-bold mb-4 text-primary font-serif">Enter Your Code</h1>
+            <Suspense>
+                <VerifyPhoneDescription />
+            </Suspense>
+        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <VerifyPhoneForm />
+        </Suspense>
+      </div>
+  );
+}
+
+function VerifyPhoneDescription() {
+    const searchParams = useSearchParams();
+    const phone = searchParams.get('phone');
+    return (
+        <p className="text-muted-foreground text-lg font-light">
+          We sent a 6-digit code to {phone}.
+        </p>
+    )
+}
