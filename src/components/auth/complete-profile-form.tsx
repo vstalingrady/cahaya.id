b037d227350b19875e5c0d5aca5cc8dcd84068f9
Vstalin Grady <VstalinGrady@gmail.com>
2025-07-04 16:19:56 +0000

    NO TURN IT BACK WHAT I MEAN ADDING A MOCK FILE IS FOR THE ICONS PUT IT B

--- src/components/auth/complete-profile-form.tsx
+++ src/components/auth/complete-profile-form.tsx
@@ -23,6 +23,8 @@ import { completeUserProfile } from '@/lib/actions';
 import { User as UserIcon, Mail, Lock, Loader2, Check, X, Eye, EyeOff } from 'lucide-react';
 import { Separator } from '../ui/separator';
 import { cn } from '@/lib/utils';
+import GoogleIcon from '@/components/icons/google-icon';
+import AppleIcon from '@/components/icons/apple-icon';
 
 function SubmitButton({ pending }: { pending: boolean }) {
   return (
@@ -35,18 +37,6 @@ function SubmitButton({ pending }: { pending: boolean }) {
     </Button>
   );
 }
-
-const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.854 3.188-1.782 4.134-1.135 1.135-2.933 2.544-5.977 2.544-4.557 0-8.28-3.72-8.28-8.28s3.723-8.28 8.28-8.28c2.49 0 4.307.975 5.704 2.345l-2.757 2.757c-.823-.78-1.89-1.32-3.03-1.32-2.75 0-5.02 2.26-5.02 5.02s2.27 5.02 5.02 5.02c3.13 0 4.61-2.26 4.82-3.55h-4.82v.003Z" fill="currentColor"/>
-  </svg>
-);
-
-const AppleIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-1.84.043-3.48 1.2-4.425 3.014-1.898 3.637-.495 8.926 1.45 11.97C6.13 22.313 7.333 24 9.13 23.94c1.613-.057 2.115-1.002 3.824-1.002s2.07.945 3.824 1.002c1.753.057 2.883-1.657 3.77-3.023.96-1.532 1.27-3.024 1.27-3.083 0-.028-2.227-1.38-2.255-4.116-.027-2.793 1.896-3.935 2.14-4.144-.99-.95-2.52-1.07-3.02-.91-.848.243-1.613.736-2.17.736zM15.19 4.31C14.34-.178 12.064 0 12.064 0s-.848 2.362.057 3.29c.904.928 2.32 1.27 3.02.822.058 0 .058-.028.058-.028s-.113-1.628-.006-2.07z" fill="currentColor"/>
-    </svg>
-);
 
 function PasswordRequirements({ password }: { password: string }) {
   const requirements = [
---
+++ src/components/icons/google-icon.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <title>Google logo</title>
+    <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.854 3.188-1.782 4.134-1.135 1.135-2.933 2.544-5.977 2.544-4.557 0-8.28-3.72-8.28-8.28s3.723-8.28 8.28-8.28c2.49 0 4.307.975 5.704 2.345l-2.757 2.757c-.823-.78-1.89-1.32-3.03-1.32-2.75 0-5.02 2.26-5.02 5.02s2.27 5.02 5.02 5.02c3.13 0 4.61-2.26 4.82-3.55h-4.82v.003Z" fill="currentColor"/>
+  </svg>
+);
+
+export default GoogleIcon;
