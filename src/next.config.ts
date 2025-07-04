b2a6f54581a16263a3279a1ab5a8dcd65fb5047a
Vstalin Grady <VstalinGrady@gmail.com>
2025-07-04 09:06:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,40 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+   experimental: {
+    allowedDevOrigins: [
+        'https://*.cloudworkstations.dev',
+    ]
+  }
+};
+
+export default nextConfig;
