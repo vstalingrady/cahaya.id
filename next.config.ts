7c38665c9f52e09341e835e709b316fa5e8313f5
Vstalin Grady <VstalinGrady@gmail.com>
2025-07-04 17:28:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -48,15 +48,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
