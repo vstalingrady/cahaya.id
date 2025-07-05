31597d443344d9e7e230c34ce04fafb8fc6268e0
Aurelryo Oscar Jonathan <aurelryojonathan@gmail.com>
2025-07-05 17:37:13 +0000

    <svg height="1em" style="flex:none;line-height:1" viewBox="0 0 24 24" wi

--- src/components/icons/GeminiLogo.tsx
+++ src/components/icons/GeminiLogo.tsx
@@ -1,62 +1,27 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 
-const GeminiLogo = ({ className, size = 56 }: { className?: string, size?: number | string }) => (
+const GeminiLogo = ({ className, size = 56 }: { className?: string; size?: number | string }) => (
   <svg
-    fill={'none'}
-    shapeRendering={'geometricPrecision'}
-    width={size}
     height={size}
+    width={size}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
     className={cn(className)}
-    xmlns={'http://www.w3.org/2000/svg'}
-    viewBox={'0 0 100 100'}
   >
+    <title>Gemini</title>
     <defs>
-      <linearGradient
-        gradientUnits={'userSpaceOnUse'}
-        id={'gemini-gradient-a'}
-        x1={22.0003}
-        x2={89.9203}
-        y1={7.5002}
-        y2={54.2602}
-      >
-        <stop offset={0.05} stopColor={'#9B5DE5'} />
-        <stop offset={1} stopColor={'#F15BB5'} />
-      </linearGradient>
-      <linearGradient
-        gradientUnits={'userSpaceOnUse'}
-        id={'gemini-gradient-b'}
-        x1={6.5}
-        x2={74.42}
-        y1={50}
-        y2={96.76}
-      >
-        <stop offset={0.16} stopColor={'#00BBF9'} />
-        <stop offset={0.87} stopColor={'#3461FD'} />
-      </linearGradient>
-      <linearGradient
-        gradientUnits={'userSpaceOnUse'}
-        id={'gemini-gradient-c'}
-        x1={64.5}
-        x2={95.5}
-        y1={64}
-        y2={93.5}
-      >
-        <stop offset={0.34} stopColor={'#00F5D4'} />
-        <stop offset={1} stopColor={'#3461FD'} />
+      <linearGradient id="lobe-icons-gemini-fill" x1="0%" x2="68.73%" y1="100%" y2="30.395%">
+        <stop offset="0%" stopColor="#1C7DFF" />
+        <stop offset="52.021%" stopColor="#1C69FF" />
+        <stop offset="100%" stopColor="#F0DCD6" />
       </linearGradient>
     </defs>
     <path
-      d={
-        'M50 7.5a6.25 6.25 0 0 0-4.42 1.83l-34.16 34.16A6.25 6.25 0 0 0 13.25 50h73.5A6.25 6.25 0 0 0 82.33 39.07l-27.91-27.9A6.25 6.25 0 0 0 50 7.5Z'
-      }
-      fill={'url(#gemini-gradient-a)'}
+      d="M12 24A14.304 14.304 0 000 12 14.304 14.304 0 0012 0a14.305 14.305 0 0012 12 14.305 14.305 0 00-12 12"
+      fill="url(#lobe-icons-gemini-fill)"
+      fillRule="nonzero"
     />
-    <path
-      d={'M13.25 50a6.25 6.25 0 0 0-1.83 4.42v27.91a6.25 6.25 0 0 0 10.68 4.42l34.16-34.16A6.25 6.25 0 0 0 50 50H13.25Z'}
-      fill={'url(#gemini-gradient-b)'}
-    />
-    <path d={'M50 50h36.75a6.25 6.25 0 0 1 4.42 10.68l-34.16 34.16A6.25 6.25 0 0 1 50 92.5V50Z'} fill={'url(#gemini-gradient-c)'} />
   </svg>
 );
 
