a5ac05864e32604155e8b03f1d88f6548df033f2
Vstalin Grady <vstalingrady@gmail.com>
2025-07-23 21:01:18 +0700

    fix: configure Capacitor to handle internal navigation properly

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,12 +6,12 @@ const config: CapacitorConfig = {
   appName: 'Cahaya',
   webDir: 'out',
   bundledWebRuntime: false,
-  server: {
-    // Connect to your development server for hot reload
-    url: 'http://192.168.1.10:3000',
-    cleartext: true,
-    allowNavigation: ['192.168.1.10:3000', 'localhost:3000']
-  },
+  // server: {
+  //   // Connect to your development server for hot reload
+  //   url: 'http://192.168.1.10:3000',
+  //   cleartext: true,
+  //   allowNavigation: ['192.168.1.10:3000', 'localhost:3000', '*']
+  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
@@ -21,6 +21,9 @@ const config: CapacitorConfig = {
     StatusBar: {
       style: 'dark',
       backgroundColor: '#1e293b'
+    },
+    App: {
+      handleOpenUrl: false
     }
   }
 };
